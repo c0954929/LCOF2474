14fd6410eb75908b1887d99287c16d3ce384252e
c0954929 <c0954929@mylambton.ca>
2024-12-07 11:33:57 -0500

    final changes before submission

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const link = document.createElement("link");
+    link.rel = "icon";
+    link.href = "images/favicon.png"; 
+    link.type = "image/png";
+    document.head.appendChild(link);
+});
